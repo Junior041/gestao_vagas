be769bb14efbc7fc287949df30e4f024bd88c404
Junior <ismaeljunior1508@gmail.com>
2023-12-12 11:30:34 -0300

    feat: senha do candidato criptografada

--- src/main/java/br/dev/ismaelsilva/gestao_vagas/modules/candidato/useCases/CreateCandidatoUseCase.java
+++ src/main/java/br/dev/ismaelsilva/gestao_vagas/modules/candidato/useCases/CreateCandidatoUseCase.java
@@ -4,10 +4,13 @@ import br.dev.ismaelsilva.gestao_vagas.exception.UserAlreadyExists;
 import br.dev.ismaelsilva.gestao_vagas.modules.candidato.CandidatoEntity;
 import br.dev.ismaelsilva.gestao_vagas.modules.candidato.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidatoUseCase {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private CandidatoRepository candidatoRepository;
     public CandidatoEntity execute(CandidatoEntity candidatoEntity){
@@ -15,6 +18,7 @@ public class CreateCandidatoUseCase {
                 .ifPresent((user) -> {
                     throw new UserAlreadyExists();
                 });
+        candidatoEntity.setPassword(this.passwordEncoder.encode(candidatoEntity.getPassword()));
         return this.candidatoRepository.save(candidatoEntity);
     }
 }
